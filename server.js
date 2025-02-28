d8d895caf1d372650d732796f76a81a6c4bfa78a
fennotype <bogdanovDV15@gmail.com>
2025-02-28 20:59:28 +0300

    finaly

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const pool = require(`./db`)
 const fetchReposInfo = require(`./repositories`)
 const express = require(`express`)
+const path = require('path')
+const cors = require(`cors`)
 const app = express()
 const PORT = 3000
+
+
+app.use(cors()); 
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/repositories', (req, res) => {
+    res.sendFile(path.join(__dirname, '', 'index.html')); // Путь к вашему HTML файлу
+})
 
 app.get(`/repositories`, async (req, res) => {
     try {
@@ -15,20 +25,28 @@ app.get(`/repositories`, async (req, res) => {
 
 app.get(`/repositories/:ReposIdOrLanguage`, async (req,res)=>{
     const {ReposIdOrLanguage} = req.params;
+    console.log(`Запрос на: /repositories/${ReposIdOrLanguage}`);
     let result
     try{
         if(!isNaN(ReposIdOrLanguage)){
+            //поиск по id
             result = await pool.query(`SELECT * FROM repositories WHERE id=$1`,[ReposIdOrLanguage])
         }else{
-            result = await pool.query(`SELECT * FROM repositories WHERE language=$1`,[ReposIdOrLanguage])
+            //поиск по имени
+            result = await pool.query(` SELECT * FROM repositories  WHERE LOWER(name) LIKE LOWER($1) OR LOWER(language) LIKE LOWER($1)`,[`%${ReposIdOrLanguage}%`]);  
         }
-        if(result.rows.length > 0 ){
-            res.json(result.rows)
-        } else{
-            res.json({Message:`репозиторий не найден`})
+        console.log('Результаты из базы данных:', result.rows);
+
+        if (result.rows.length > 0) {
+            // Если есть результаты, отправляем их
+            res.json(result.rows);
+        } else {
+            // Если нет результатов, отправляем сообщение
+            res.json({ Message: `Репозиторий не найден` });
         }
-    }catch(error){
-        console.error(error)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ Message: 'Ошибка сервера' });
     }
 })
 
@@ -48,7 +66,7 @@ app.get(`/repositories/:ReposIdOrLanguage`, async (req,res)=>{
     //         console.error(`syncInterval failed` + ` ` + error)
     //     }
     // })
-startSyncInterval()
+// startSyncInterval()
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}/repositories`);
